4153754403d25caf0bd8cdc78ab7118b637c0442
Naveen <nayaknaveen445.com>
2025-05-09 20:02:32 +0530

    Update day1.py with new changes

--- Learning_Python_Daily/Day_01.py
+++ Learning_Python_Daily/Day_01.py
@@ -1,27 +1,27 @@
-# # Using f-strings as formated output
-# name="Naveen"     #'name' is a variable that stores the value 'Naveen'
-# age=25            # variables can store multiple values and whatever data types such as int,bool,float,string 
-# print(f"My name is {name} and my age is {age}")
+# Using f-strings as formated output
+name="Naveen"     #'name' is a variable that stores the value 'Naveen'
+age=25            # variables can store multiple values and whatever data types such as int,bool,float,string 
+print(f"My name is {name} and my age is {age}")
 
-# #let's create a simple project: "User Profile"
-
-
-# #user profile generator
-# n="Naveen"        #string
-# ag=25           #int
-# height=5.9      #in feet and float
-# is_student=True #boolean
+#let's create a simple project: "User Profile"
 
 
-# #print profile info
-# print("\n\n---------------- User Profile -----------------\n")
-# print("Name: ",n)
-# print("Age: ",ag)
-# print("Height: ",height)
-# print("Is Student?",is_student)
+#user profile generator
+n="Naveen"        #string
+ag=25           #int
+height=5.9      #in feet and float
+is_student=True #boolean
 
-# #formatted version
-# print(f"\n\n{n} is {ag} years old and {height} feet tall. Student Status: {is_student}\n\n")
+
+#print profile info
+print("\n\n---------------- User Profile -----------------\n")
+print("Name: ",n)
+print("Age: ",ag)
+print("Height: ",height)
+print("Is Student?",is_student)
+
+#formatted version
+print(f"\n\n{n} is {ag} years old and {height} feet tall. Student Status: {is_student}\n\n")
 
 
 #2nd project: User Profile by taking input from user
