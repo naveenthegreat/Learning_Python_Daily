b8d43394d4ed3360a0f85322b9d92c5fd2a4153f
Naveen <nayaknaveen445.com>
2025-05-11 17:02:45 +0530

    A project file 'Tuples and Sets' is been added

---
+++ Python_Mini_Projects/Day_06.py
@@ -0,0 +1,15 @@
+#Mini Challenge: Unique Words Analyzer
+
+l=input("\nEnter Your Sentence: ")
+print(l)
+
+l2=str.split(l)
+l1=tuple(l2)
+print("\nOriginal Words: ",l1)
+
+l3=set(l1)
+print("Unique Words: ",l3)
+
+
+print("Total Unique Words: ",len(l3))
+
