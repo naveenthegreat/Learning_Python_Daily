88a1658f0015af649b5de229daaa177fce3303e2
Naveen <nayaknaveen445.com>
2025-06-05 20:33:06 +0530

    Datetime(Module)

--- Learning_Python_Daily/Day_10.py
+++ Learning_Python_Daily/Day_10.py
@@ -14,34 +14,34 @@ Python’s datetime module lets you:
 
 1. Importing datetime:
 
-    First install module using "pip". For ex.: pip install datetime
+    First install module using "pip" For ex.: pip install datetime
     Then import: "import datetime"
 
-2. Get current and time:
+2. Get current date and time:
 
     today=datetime.date.today()
     now=datetime.datetime.now()
-    print(today)
-    print(now)
+    print(today)                       # 2025-06-05
+    print(now)                         # 2025-06-05 16:47:23.220545
 
 3. Access Data Components: 
 
-    print(today.month)  # 5
-    print(today.day)    # 17
-    print(today.year)   # 2025
+    print(today.month)                 # 6
+    print(today.day)                   # 5
+    print(today.year)                  # 2025
 
 4. Formats Dates as Strings:
 
 formatted = today.strftime("%d-%m-%Y")   # "17-05-2025"
 
-Code                Output                  Meaning
-
-%Y and %y        2025 and 25          4 digit and 2 digit
-%m                   05                     Month   
-%d                   02                      Day
-%A                 Monday                Full Weekday
-%H and %I        14 and 02               24 hr and 12 hr   
-%p                  PM                      AM/PM
+Code         |        Output          |             Meaning
+             |                        |
+%Y and %y    |    2025 and 25         |    4 digit and 2 digit
+%m           |         06             |           Month   
+%d           |         05             |           Day
+%A           |      Monday            |       Full Weekday
+%H and %I    |    14 and 02           |      24 hr and 12 hr   
+%p           |        PM              |          AM/PM
 
 5. Time Differences (timedelta):
 
@@ -51,7 +51,24 @@ Code                Output                  Meaning
     yesterday = today - timedelta(days=1)
     next_week = today + timedelta(days=7)
 
-    print(yesterday)
-    print(next_week)
+    print(yesterday)                     # 2025-06-04
+    print(next_week)                     # 2025-06-12
 
+ 6. Parse Date Strings to datetime:
+
+ Convert string → datetime using strptime():
+
+ import datetime
+ date_string = "2025-05-6"
+ parsed = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+ print(parsed.date())
+
+ 
+ 7. Timing How Long Something Takes:
+
+ start=datetime.datetime.now()
+ end=datetime.datetime.now()
+
+ duration = end - start
+ print("Took: ",duration )
 '''
