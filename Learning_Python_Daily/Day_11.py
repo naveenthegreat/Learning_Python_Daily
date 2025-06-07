9f26132182b9672e2b3dabbc65ffbc515d4d98fd
Naveen <nayaknaveen445.com>
2025-06-07 14:52:49 +0530

    Modules & Packages

---
+++ Learning_Python_Daily/Day_11.py
@@ -0,0 +1,22 @@
+# Modules & Packages
+'''
+✅ What Is a Module?
+    A Module is a simply .py file which is written by other and that can be reuse
+    by us.
+
+🗂 What is a Package?
+    A package is a folder containing a group of related modules, with an __int__.py 
+    file inside.
+
+Built in module:
+    datetime      Handle date-times
+    os            work with files and folders
+    json          save and load structured data
+    math          Advanced math functions
+    random        Generate random numbers   
+
+Install extra modules with pip:
+    "pip install datetime"    
+
+'''
+import Day_10
