aad9b6ab9ea7371ee31d2bd0b92ba33e4dff335f
Naveen <nayaknaveen445.com>
2025-06-07 11:14:03 +0530

    Datetime(Module)

--- Python_Mini_Projects/Day_10.py
+++ Python_Mini_Projects/Day_10.py
@@ -4,20 +4,30 @@ import datetime
 from datetime import timedelta
 
 start=datetime.datetime.now()
-date_=input("Enter the last date: ")
+
+date_=input("When did you last study Python? (DD-MM-YY): ")
 par=datetime.datetime.strptime(date_,"%d-%m-%y")
 last=par.date()
 
 today=datetime.date.today()
 format=today.strftime("%A, %d %B %y")
-print("\nToday: ",format)
+print("\n📅 Today: ",format)
 
 Last_study=par.strftime("%A, %d %B %y")
-print("Last Studied: ",Last_study)
+print("🧠 Last Studied: ",Last_study)
 
-yesterday = (today-par.date()).days
-print("Days: ",yesterday)
+days_gap = (today-par.date()).days
+print("Days: ",days_gap)
 
+if days_gap==0:
+    print("\n✅ You're on a track")
+
+elif days_gap==1:
+    print("\n🔥 Your Streak Is Alive")
+
+else:
+    print(f"\n⏳ It has been {days_gap} day(s) since your last study session.")
+    print("⛔ Streak broken!")
 
 end=datetime.datetime.now()
 
