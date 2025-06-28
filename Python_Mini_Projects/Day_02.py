62134597683cfa7dfbfa547e2304c20669ce07fd
Naveen <nayaknaveen445.com>
2025-06-28 09:24:02 +0530

    Operators

---
+++ Python_Mini_Projects/Day_02.py
@@ -0,0 +1,11 @@
+#let's start mini project: Calculator program (with Expressions)
+
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+
+messages=["small","big"]
+
+print(messages[(a + b) < 100])
+a = 10
+a *= 2 + 3
+print(a)
