8de421428b7c1ad6dfe600de56e4b28850e73db0
Naveen <nayaknaveen445.com>
2025-07-03 18:49:34 +0530

    Inheritence (Oops 1st Pillar)

---
+++ Learning_Python_Daily/Day_14.py
@@ -0,0 +1,61 @@
+# Inheritence (Oops 1st Pillar)
+'''
+1. What is Inheritence?
+    Inheritence means a class(child) can use code of another class(parents).
+
+Ex.: 
+class animal:
+    def sound(self):
+        print("Animals sounds different and some animals crazy")
+
+class dog(animal):
+    def bark(self):
+        print("Dog bark as bow!! bow!!!")
+
+d=dog()
+d.sound()                       #from parent class
+d.bark()                        #from child class
+
+
+2. Types Of Inheritence:
+
+Type                        Descriptions                            Examples
+
+Single                  1 parent, 1 child                       class B(A)
+Multiple               multiple parents                         class C(A, B)
+Multilevel              Grandparent-> Parent-> Child            class C(B)
+Hierarchical            1 parent, many child                    class B(A), class C(A)  
+
+3. super() Method :
+    super() is used inside a child class to call methods or constructors from the parent
+    class.
+    Mainly used when you override the __inti__ methods or other methods.
+
+Ex.:
+
+class Animal:
+    def __init__(self):
+        print("Animal Created")
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()  # Calls Animal's constructor
+        print("Dog Created")
+
+4. Method Overriding :
+    If a child class has a method with the same names as in the parent, it 
+    overrides it.
+
+Ex.:
+
+class animal:
+    def sound(self):
+        print("Sound is generic")
+
+class cat(animal):
+    def sound(self):
+        print("Meow Meow")
+
+c=cat()
+c.sound()
+''' 
---
+++ Python_Mini_Projects/Day_14.py
@@ -0,0 +1,17 @@
+# Mini Challenge:
+
+class person:
+    name = "Naveen"
+    age = 23
+    def introduce(self):
+        print("He is genius employer")
+    
+class employee(person):
+    job_title = "DevOps Engineer"
+    salary = 50,00,000                      #50,00,000 ❌ 5000000 or 5_000_000 ✅
+    def introduce(self):
+        super().introduce()          
+    
+e=employee()
+e.introduce()
+
