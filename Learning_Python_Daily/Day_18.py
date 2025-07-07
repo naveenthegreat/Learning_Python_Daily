15eae716d545f9a132c39b6bdda43d6514a52706
Naveen <nayaknaveen445.com>
2025-07-07 13:29:50 +0530

    Tkinter (Layouts)

--- Learning_Python_Daily/Day_18.py
+++ Learning_Python_Daily/Day_18.py
@@ -3,35 +3,36 @@
 Your app window is like an empty canvas. Layout managers decides where each button,label or box 
 will appear.
 
+
 Tkinter has 3 layout systems :
 
-1 .pack()  -> vertical/horizontal stacking (basic layout)
-
-    Auto-stakcs widgets vertically (top to bottom) or horizontally (left to right)
+1 .pack()  -> vertical/horizontal stacking (basic layout):
+    "Auto-stakcs widgets vertically (top to bottom) or horizontally (left to right)"
 
 Syntax: 
         widget.pack(side="top", fill="x", padx=10, pady=5)
-Here: 
-        fill      = x,y both - fills horizontally or vertically
-        padx/pady = adds spacing around widget
+      
+        Here: 
+              fill      = x,y both - fills horizontally or vertically
+              padx/pady = adds spacing around widget
 
     
+        
 2 .grid()  -> table-based positioning (rows and columns)
-
-    Arrange widgets in rows and columns (like Excel or calender grid)
+    "Arrange widgets in rows and columns (like Excel or calender grid)"
 
 Syntax:
         widget.grid(row=0, column=1, padx=5, pady= 10)
-Here:
-        columnspan, rowspan	= Stretch across multiple cells
+        
+        Here:
+              columnspan, rowspan	= Stretch across multiple cells
         
 
+        
 3 .place() -> absolute positioning (x,y)
-
-    Place widgets at exact x, y coodinates.
+    "Place widgets at exact x, y coodinates."
 
 Syntax:
-        widgets.place(x=100, y=200)         # Use only for small fine-tuning or special effects (not for full layout).
-
+        widgets.place(x=100, y=200)                    # Use only for small fine-tuning or special effects (not for full layout).
 
 '''
