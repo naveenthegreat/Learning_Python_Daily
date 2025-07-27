9ba298f3f0e86f3291f1aa274193bdb3a32db2bd
Naveen <nayaknaveen445.com>
2025-07-27 17:21:07 +0530

    ok

---
+++ My_Diary/app.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import messagebox
+import json
+from datetime import datetime
+import os
+
+# ========== Window Setup ==========
+window = tk.Tk()
+window.title("📝 My Daily Diary")
+window.geometry("500x500")
+window.config(bg="#f8f4e3")
+
+
+
+# ========== Run the App ==========
+window.mainloop()
