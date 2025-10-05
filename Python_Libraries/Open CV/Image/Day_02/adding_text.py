02abc026a9c2c825580b2c9a02ed452affeff63c
Naveen <nayaknaveen445.com>
2025-10-05 16:16:21 +0530

    Cheatsheet

--- Python_Libraries/Open CV/Image/Day_02/adding_text.py
+++ Python_Libraries/Open CV/Image/Day_02/adding_text.py
@@ -3,13 +3,13 @@ import cv2
 image = cv2.imread(r"C:\Users\NAVEEN\OneDrive\Desktop\Python_Project\Learning_Python_Daily\Python_Libraries\Open CV\Image\Day_02\pokemon.jpg")
 
 if image is not None:
-    print("Image Loaded")
-    resized = cv2.resize(image,(500,400))
-    cv2.imshow("Original_Photo",image)
-    cv2.imshow("Resized_photo",resized)
+    print("Image loaded Successfully")
+    cv2.putText(image,"Hello N.K.Nayak k fans",(50,80),
+                cv2.FONT_HERSHEY_COMPLEX,1.2,(0,235,230),3)
+
+    cv2.imshow("Image",image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 else:
-    print("Image not loaded successfully")
-
+    print("Image is not loaded")
--- Python_Libraries/Open CV/Image/Day_02/flipped.py
+++ Python_Libraries/Open CV/Image/Day_02/flipped.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread(r"C:\Users\NAVEEN\OneDrive\Desktop\Python_Project\Learning_Python_Daily\Python_Libraries\Open CV\Day_02\pokemon.jpg")
+image = cv2.imread(r"C:\Users\NAVEEN\OneDrive\Desktop\Python_Project\Learning_Python_Daily\Python_Libraries\Open CV\Image\Day_02\pokemon.jpg")
 
 if image is not None:
     print("Image loaded successfully")
